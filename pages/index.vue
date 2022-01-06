a2deeda9d82af00b4aec4b77e3b2ab53a0d8b570
valhugnat1 <hugo.philipp99@gmail.com>
2022-01-06 14:29:30 +0100

    versionTest1-Qrcode-downloadZip-firebaseConnect

--- pages/index.vue
+++ pages/index.vue
@@ -92,9 +92,9 @@ export default {
       var charactersLength = characters.length;
       for ( var i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
     }
-    return result;
-  }
   }
 }
 </script>
